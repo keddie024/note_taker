be13eb3b62addbfc9a362e8d75d308062ad1ab0b
Kevin Eddie <keddie024@gmail.com>
2021-04-21 17:49:00 -0400

    added express routes for get and post

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
-const express = require ("express");
-const path = require ("path");
-const fs = require ("fs");
-const noteList = require ("./db/db.json");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const noteList = require("./db/db.json");
 const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+
+app.get("/api/notes", (req, res) => {
+    return res.json(JSON.parse(fs.readFileSync("./db/db.json")));
+});
+
+app.post("/api/notes", (req, res) => {
+    const note = req.body;
+
+    noteList.push(note);
+    note.id = noteList.indexOf(note);
+    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+
+    res.json(note);
+})
 
 
 
