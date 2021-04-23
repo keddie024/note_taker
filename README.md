5a0c0448f60e84a9775e5a83035b32347a916c8a
Kevin Eddie <keddie024@gmail.com>
2021-04-23 14:48:33 -0400

    updated readme and added comments

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # note_taker
+
+## 4/23/21
+
+This application allows a user to write and save notes. The note will have a title to identify it as well as the text the user wishes to save. Upon saving a note, it will appear on the left side of the page and can be clicked. Upon clicking a note, the main body of the page will update to display what the user had saved.
+
+## Application Link
+
+https://note-taker-keddie024.herokuapp.com/
+
+## Application Images
+
+![Main Page](/images/sample1.png)
+
+#
+
+![Note Page](/images/sample2.png)
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Dependencies
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
@@ -10,11 +11,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-
+// Reads the note data that is currently stored.
 app.get("/api/notes", (req, res) => {
     return res.json(JSON.parse(fs.readFileSync("./db/db.json")));
 });
 
+// Adds note data that the user creates.
 app.post("/api/notes", (req, res) => {
     let note = req.body;
 
@@ -25,12 +27,14 @@ app.post("/api/notes", (req, res) => {
     res.json(note);
 })
 
-
+// Will bring users to the home page.
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
+// Will bring users to the notes page.
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
+// Will bring users to the home page if they type in a bad path.
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
-
+// Creates the server and begins waiting for directions.
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
